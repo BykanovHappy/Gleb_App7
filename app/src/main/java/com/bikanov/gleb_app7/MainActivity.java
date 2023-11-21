78874d4d8b60c7beb8d7b840fe4d5d2926a40d2b
Gleb <bykanovgleb123@gmail.com>
2023-11-21 16:36:30 +0300

    Initial commit

---
+++ app/src/main/java/com/bikanov/gleb_app7/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bikanov.gleb_app7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
